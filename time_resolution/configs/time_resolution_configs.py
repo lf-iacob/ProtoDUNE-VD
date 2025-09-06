f2472416f18bf5e307b5625aa3122e8a911a533b
lf-iacob <iacob.laura.f@gmail.com>
2025-09-06 15:48:29 +0200

    Create time_resolution_configs.py

---
+++ time_resolution/configs/time_resolution_configs.py
@@ -0,0 +1,7 @@
+# Data folder
+data_folder: "/eos/experiment/neutplatform/protodune/experiments/ProtoDUNE-VD/commissioning/"
+
+# Ana folder
+ana_folder : "/afs/cern.ch/work/l/liacob/public/time_resolution/"
+raw_ana_folder: "raw_ana/"
+single_ana_folder: "single_ana/"
